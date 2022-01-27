1942a99e3df90b474744d3a760b50d0a5f03c78f
Oxana Danilova <oxanadanilova11@gmail.com>
2022-01-27 21:18:10 +0100

    feat: choose card

--- app.js
+++ app.js
@@ -1,39 +1,79 @@
-const cutArr = (arr, num)=> {
-arr.splice(num, 1);
-console.log('result arr', arr);
+const displaySelectedCard = (num) => {
+  const arrCards = document.querySelectorAll(".active");
+  arrCards.forEach((el, index) => {
+    if (index === num) {
+      el.classList.remove("active");
+      el.classList.add("selected");
+    }
+  });
+};
 
+const makeInactive = () => {
+  const selectedCard = document.querySelector(".selected");
+  if (selectedCard) {
+    selectedCard.classList.remove("selected");
+    selectedCard.classList.add("inactive");
+  }
+};
+
+const disableChooseBtn = ()=> {
+    const chooseBtn = document.querySelector('.choose');
+    chooseBtn.disabled = true;    
 }
 
-const randomFunc = (arr)=> {
-    console.log('length', arr.length);
-    console.log('arr', arr);
-    let randomNumb = Math.floor(Math.random()*arr.length);
-   
-    console.log('random Number', randomNumb);
-    console.log('random user', arr[randomNumb]);
+const enableChooseBtn = ()=> {
+    const chooseBtn = document.querySelector('.choose');
+    chooseBtn.disabled = false;
+}
+
+const cutArr = (arr, num) => {
+  arr.splice(num, 1);
+  console.log("result arr", arr);
+};
+
+const randomFunc = (arr) => {
+  console.log("length", arr.length);
+  console.log("random func arr", arr);
+  makeInactive();
+  if (arr.length) {
+    let randomNumb = Math.floor(Math.random() * arr.length);
+
+    console.log("random Number", randomNumb);
+    console.log("random user", arr[randomNumb]);
 
     cutArr(arr, randomNumb);
+    displaySelectedCard(randomNumb);
+  } else {
+    disableChooseBtn();
+    console.log("Finish!");
+  }
+};
 
-}
+const renderArr = (arr) => {
+  const main = document.querySelector("main");
+  main.innerHTML = '';
+  const ul = document.createElement("ul");
+  arr.forEach((el) => {
+    const li = document.createElement("li");
+    li.innerHTML = el;
+    li.classList.add("active");
+    ul.appendChild(li);
+  });
+  main.appendChild(ul);  
+};
 
-const renderArr = (arr)=> {
-    const main = document.querySelector('main');
-    console.log(main);
-    const ul = document.createElement('ul');
-    arr.forEach((el)=>{
-        const li = document.createElement('li');
-        li.innerHTML = el;
-        ul.appendChild(li);
-    });
-    console.log(ul);
-    main.appendChild(ul);
+const arrCreate = () => {
+  const arr = ["vera", "alex", "petr", "gregor", "anna"];
+  renderArr(arr);
+  const chooseBtn = document.querySelector(".choose");
+  chooseBtn.addEventListener("click", () => randomFunc(arr));
+  enableChooseBtn();
+  // randomFunc(arr);
+};
 
-}
+const app = () => {
+  const startBtn = document.querySelector(".start");
+  startBtn.addEventListener("click", arrCreate);  
+};
 
-const arrCreate = ()=> {
-    const arr = ['vera', 'alex', 'petr', 'gregor', 'anna'];
-    renderArr(arr);
-   // randomFunc(arr);
-}
-
-arrCreate();
+app();
